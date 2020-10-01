93897badde45fa715fe9101cabbcf4461c9fab75
JEudy89 <sitekhugo@gmail.com>
2020-10-01 11:38:02 -0400

    finished task 3 and 4

--- index.js
+++ index.js
@@ -1,16 +1,16 @@
 /************************************************************** Task 1: Warm-up! **************************************************************/
 //Task a: declare a variable called votingAge, console log true if age > 18 (no function required)
-const votingAge = 18
-if (votingAge >= 18){
-    console.log(true);
-} else {
-    console.log(false);
-} 
+// const votingAge = 18
+// if (votingAge >= 18){
+//     console.log(true);
+// } else {
+//     console.log(false);
+// } 
 
 
 
 //Task b: declare a variable and then use a conditional to change the value of that variable based on the value assigned to a second variable (no function required)
-let store = "Belks"
+let store = "Wally"
 let price = 10
 
 if (store != "Belks") {
@@ -26,10 +26,10 @@ console.log(store);
 
 
 //Task c: Convert string ("1999") to integer (1999)  (no function required) // hint look up the Number method
-let num = "1999"
-console.log(num);
-Number(num);
-console.log(num);
+// let num = "1999"
+// console.log(num);
+// Number(num);
+// console.log(num);
 
 
 
@@ -40,19 +40,28 @@ console.log(num);
 // }
 // console.log(multiply(2, 4));
 
-const multiply = (a, b) => {
-    return a * b;
-}
-console.log(multiply(5, 5));
+// const multiply = (a, b) => {
+//     return a * b;
+// }
+// console.log(multiply(5, 5));
 
 /************************************************************** Task 2 **************************************************************/
 //Age in Dog years
 //write a function that takes your age and returns it to you in dog years - they say that 1 human year is equal to seven dog years 
 
-
-
-
-
+// function dogAge (age) {
+//     return age * 7;
+// }
+// const dogAge = (age) => {
+//     return age * 7;
+// }
+//  let dogAge1 = (age) => {
+//      return age * 7
+//  }
+// console.log(dogAge(5))
+// console.log(dogAge1(6))
+// dogAge1 = 10;
+// console.log(dogAge1)
 /************************************************************** Task 3 **************************************************************/
 //Dog feeder 
 //takes weight in pounds and age in years (note if the dog is a puppy the age will be a decimal) and returns the number of pounds of raw food to feed in a day.
@@ -72,8 +81,28 @@ console.log(multiply(5, 5));
 // when you are finished invoke your function with the weight of 15 lbs and the age of 1 year - if your calculations are correct your result should be 0.44999999999999996
   
 
-
-
+function food(age, weight) {
+if (age >= 1) {
+    if (weight <= 15 && weight >= 11) {
+    console.log(weight * .03);
+    } else if (weight > 15){
+        console.log(weight * .02);
+    } else if (weight >= 6 && weight <=10) {
+        console.log(weight * .04);
+    } else if (weight < 5) {
+        console.log(weight * .05);
+    }
+} else if (age < 1) {
+    if (age > 2/12 && age < 4/12) {
+        console.log(weight * .10)
+    } else if (age > 4/12 && age < 7/12) {
+        console.log(weight * .05);
+    } else {
+        console.log(weight * .04)
+    }
+}
+}
+console.log(food(1,15))
 
 /************************************************************** Task 4 **************************************************************/
 // Rock, Paper, Sissors
@@ -82,7 +111,35 @@ console.log(multiply(5, 5));
 // use math.random to determine the computers choice 
 // hint while you can complete this with only conditionals based on strings it may help to equate choice to a number 
 
-  
+// Note: I am giving "Paper" 0-0.32, "Scissors" 0.33-0.65, and "Rock" 0.66-0.99
+  let myHand = "Rock"
+  let opponentHand = Math.random()
+
+  if (myHand = "Scissors") {
+      if (opponentHand >= 0 && opponentHand <= 0.32) {
+          console.log("You Win!");
+      } else if (opponentHand >= 0.33 && opponentHand <= 0.65) {
+          console.log("Draw!");
+      } else {
+          console.log("You Lose!");
+      }
+  }  else if (myHand = "Paper") {
+    if (opponentHand >= 0 && opponentHand <= 0.32) {
+        console.log("Draw!");
+    } else if (opponentHand >= 0.33 && opponentHand <= 0.65) {
+        console.log("You Lose!");
+    } else {
+        console.log("You Win!");
+    }
+} else if (myHand = "Rock") {
+    if (opponentHand >= 0 && opponentHand <= 0.32) {
+        console.log("You Lose!");
+    } else if (opponentHand >= 0.33 && opponentHand <= 0.65) {
+        console.log("You Win!");
+    } else {
+        console.log("Draw!");
+    }
+}
   
 
 /************************************************************** Task 5 **************************************************************/
