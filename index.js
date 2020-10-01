16dec1109b03db4648ddbe9895447e7e2f585f4a
JEudy89 <sitekhugo@gmail.com>
2020-10-01 13:07:33 -0400

    finished the tasks, have not yet worked on the stretch goals

--- index.js
+++ index.js
@@ -187,9 +187,20 @@ console.log(annoyingSong(99));
 //60s should be D 
 //and anything below 60 should be F
   
-
-  
-  
+function gradeCalculator(grade) {
+    if (grade >= 90){
+        return "A";
+}   else if (grade >= 80 && grade < 90){
+    return "B";
+}   else if (grade >= 70 && grade < 80){
+    return "C";
+}   else if (grade >= 60 && grade < 70){
+    return "D";
+}   else {
+    return "F";
+}
+}
+  console.log(gradeCalculator(45))
 
 /************************************************************** Stretch **************************************************************/
 //Create a function that counts the number of vowels within a string. It should handle both capitalized and uncapitalized vowels.
